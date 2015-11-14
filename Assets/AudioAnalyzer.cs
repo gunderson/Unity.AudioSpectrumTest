4bafa3f72d2c574e1ecd44deabd4d21fc377078a
gunderson <pat@theorigin.net>
2015-11-13 16:16:08 -0800

    initial commit

---
+++ Assets/AudioAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioAnalyzer : MonoBehaviour {
+
+	AudioSource MainAudioSource;
+
+	// Use this for initialization
+	void Start () {
+		MainAudioSource = gameObject.GetComponent<AudioSource> ();
+		MainAudioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float[] b = new float[32];
+		MainAudioSource.GetSpectrumData(b, 0, FFTWindow.BlackmanHarris);
+	}
+}
